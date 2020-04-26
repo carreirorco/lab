ca240b820bc1b6c9111d92105df1ef9f17bd51d1
Rafael Carreiro <carreirorco@gmail.com>
2020-04-26 02:20:31 -0300

    added python examples

--- lab_python/crud_python_bottle/parte_3.py
+++
@@ -1,9 +0,0 @@
-from bottle import request, route, run, response
-
-@route('/')
-def hello_again():
-    if request.get_cookie('visited'):
-        return 'Olá, bem vindo de volta'
-    response.set_cookie('visited', 'yes')
-    return 'Olá, é um prezer conhecer voce.'
-run(port=8080)
